9098b7486059a37dc791ca120a114cff6e412dca
Maksym Shevchyk <maksim.shevchik@gmail.com>
2026-06-02 12:49:19 +0200

    Installing boxicons framework and google fonts. Header is done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import styles from './Hero.module.css'
+
+export default function Hero(){
+    return(
+        <>
+          <section className={styles.heroSection}>
+            
+          </section>
+        </>
+    )
+}
